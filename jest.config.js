1485eb6e7d5ca83a4c2f3481029893599d5330a3
jadeai.app <jadeai.app@gmail.com>
2025-10-07 07:02:44 +0000

    Changes before Firebase Studio auto-run

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+};
